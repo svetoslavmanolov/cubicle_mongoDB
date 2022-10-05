dde6306fae30620941c771b2d42b299cbeaa6681
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-05 08:50:09 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 const port = 5000;
 
 require('./config/handlebars')(app);
 
+app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('Hello there!')
-});
+
+// app.get('/', (req, res) => {
+//     res.send('Hello there!')
+// });
+
+app.use(routes);
 
 app.listen(port, () => console.log(`App is listening on port ${port}...`))
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
-
+const cubeController = require('./controllers/cubeController');
+const homeController = require('./controllers/homeController');
 
 const router = express.Router();
+
+router.use('/', homeController);
 
 
 module.exports = router;
