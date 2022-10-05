edf996dcbfc0c8c7c19647391fc42d98027fd5b8
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-05 09:22:37 +0300

    Database connection setup

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const port = 5000;
+const { initializeDatabase } = require('./config/database');
 
 require('./config/handlebars')(app);
 
 app.use('/static', express.static('public'));
 
 
-// app.get('/', (req, res) => {
-//     res.send('Hello there!')
-// });
 
 app.use(routes);
 
-app.listen(port, () => console.log(`App is listening on port ${port}...`))
+initializeDatabase()
+.then(() => {
+    app.listen(port, () => console.log(`App is listening on port ${port}...`));
+    console.log('Database is connected.')
+})
+.catch((err) => {
+    console.log('Cannot connect to DB:', err);
+})
+
 
