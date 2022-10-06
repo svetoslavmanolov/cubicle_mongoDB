06177bb1fbe6f49cfd5338bad36c0f17c7f8a713
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-06 11:39:49 +0300

    Make a db relation between cube and accessory

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -9,16 +9,16 @@ exports.getOneDetails = (cubeId) => Cube.findById(cubeId).populate('accessories'
 
 exports.create = (cube) => Cube.create(cube);
 
-// exports.attachAccessory = async (cubeId, accessoryId) => {
-//     const cube = Cube.findById(cubeId);
-//     const accessory = Accessory.findById(accessoryId);
+exports.attachAccessory = async (cubeId, accessoryId) => {
+    const cube = await Cube.findById(cubeId);
+    const accessory = await Accessory.findById(accessoryId);
 
-//     cube.accessories.push(accessory);
-//     accessory.cubes.push(cube);
+    cube.accessories.push(accessory);
+    accessory.cubes.push(cube);
 
-//     await cube.save();
-//     await accessory.save();
+    await cube.save();
+    await accessory.save();
 
-//     return cube;
-// }
+    return cube;
+}
 
