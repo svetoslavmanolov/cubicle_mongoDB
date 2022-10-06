97ada43c7f5e20f968b4cc15a957bf63353c837b
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-06 11:42:54 +0300

    Show accessories in the details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
+const accessoryService = require('../services/accessoryService');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -8,7 +9,7 @@ router.get('/create', (req, res) => {
 router.post('/create', async (req, res) => {
     const cube = req.body;
 
-    if(cube.name.length < 2) {
+    if (cube.name.length < 2) {
         return res.status(400).send('Invalid request - name must contain more than 2 letters');
     }
     try {
@@ -20,16 +21,23 @@ router.post('/create', async (req, res) => {
     }
 });
 
-// router.get('/details/:cubeId', async (req, res) => {
-//     const cube = await cubeService.getOne(req.params.cubeId);
-//     res.render('details', { cube });
-// });
-
 router.get('/details/:cubeId', async (req, res) => {
     const cube = await cubeService.getOneDetails(req.params.cubeId).lean();
     res.render('details', { cube });
 });
 
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeService.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryService.getAllAvailable(cube.accessories).lean();
+    //console.log(cube.accessories);
+    res.render('accessory/attach', { cube, accessories });
+});
+
+router.post('/:cubeId/attach-accessory', async (req, res) => {
+    const accessoryId = req.body.accessory;
+    await cubeService.attachAccessory(req.params.cubeId, accessoryId);
+    res.redirect(`/cube/details/${req.params.cubeId}`);   
+});
 
 
 
