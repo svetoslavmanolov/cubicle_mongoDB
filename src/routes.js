a84ae6d51625d3f27876bd3cfdf2f311ef37de2d
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-10 11:57:06 +0300

    Create endpoint for register page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 const router = express.Router();
 
 router.use('/', homeController);
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', authController);
 
 
 module.exports = router;
