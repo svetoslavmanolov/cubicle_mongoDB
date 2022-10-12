39c19e571f3690d1a967022903b89da637bc5a40
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-12 10:51:23 +0300

    Logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -33,4 +33,9 @@ router.post('/login', async (req, res) => {
     res.redirect('/');
 });
 
+router.get('/logout', (req, res) => {
+    res.clearCookie(sessionName);
+    res.redirect('/');
+})
+
 module.exports = router;
