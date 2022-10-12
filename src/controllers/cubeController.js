57d53df56c0f35054a810f5ae8eeb0cbf0059d37
svetoslavmanolov <svn.manolov@gmail.com>
2022-10-12 10:28:29 +0300

    Verification for owner in edit page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
 const accessoryService = require('../services/accessoryService');
-const {isAuth} = require('../middlewares/authMiddleware');
+const { isAuth } = require('../middlewares/authMiddleware');
 
 router.get('/create', isAuth, (req, res) => {
     res.render('create');
@@ -38,23 +38,31 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
 router.post('/:cubeId/attach-accessory', async (req, res) => {
     const accessoryId = req.body.accessory;
     await cubeService.attachAccessory(req.params.cubeId, accessoryId);
-    res.redirect(`/cube/details/${req.params.cubeId}`);   
+    res.redirect(`/cube/details/${req.params.cubeId}`);
 });
 
-router.get('/:cubeId/edit', async (req,res) => {
+router.get('/:cubeId/edit', isAuth, async (req, res) => {
 
-    console.log(req.user)
     const cube = await cubeService.getOne(req.params.cubeId).lean();
+
+    if (cube.owner != req.user._id) {
+        //kogato e !== nqma da raboti zashtoto ne sa ednakvi -->. A kogato e != JS go force-va do string
+        // console.log(cube.owner); --> new ObjectId("6346600109734321788dd74d")
+        //console.log(req.user._id) --> 6346600109734321788dd74d
+        return res.redirect('/404');
+    }
+
+
     cube[`difficultyLevel${cube.difficultyLevel}`] = true;
-    
-    if(!cube) {
+
+    if (!cube) {
         return res.redirect('/404')
     }
-    res.render('cube/edit', {cube});
+    res.render('cube/edit', { cube });
 });
 
 router.post('/:cubeId/edit', async (req, res) => {
-    
+
     let modifiedCube = await cubeService.edit(req.params.cubeId, req.body);
     res.redirect(`/cube/details/${modifiedCube._id}`);
 });
